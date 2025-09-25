994d1293eefcd29982c5d6decd38cce580fd25d7
Luiggi Rosas <lrosasp05@gmail.com>
2025-09-24 21:13:42 -0500

    prueba 4

---
+++ secret.py
@@ -0,0 +1,9 @@
+#generar una inyeccion sql en python
+
+def funcion_inyeccion_sql(user_input):
+    query = "SELECT * FROM users WHERE username = '" + user_input + "';"
+    return query
+
+# Ejemplo de uso
+user_input = "admin' --"
+print(funcion_inyeccion_sql(user_input))
