4b463855e8fc809909fe5291f95d1f4d870d259a
Luiggi Rosas <lrosasp05@gmail.com>
2025-09-24 21:21:05 -0500

    prueba 6

---
+++ cachorra.py
@@ -0,0 +1,9 @@
+#script de vulnerabilidad
+print("Este es un script de vulnerabilidad")
+print("No contiene vulnerabilidades")
+print("Todo es seguro")
+
+def funcion_segura():
+    return "Esta funcion es segura"
+
+print(funcion_segura())
