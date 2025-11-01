d98cd176719ba4555fb8496c1c9b16a524420eba
djjhonH01a <djjhon_15@hotmail.com>
2025-11-01 17:30:59 -0500

    prueba12

--- secrets_exposed.py
+++ secrets_exposed.py
@@ -1,2 +1,8 @@
-
-print("Hello, World fefewferferferf312312312312312!")
+def buscar_usuario_inseguro(nombre):
+    import sqlite3
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    # INSEGURO: concatenación directa permite SQL injection
+    query = "SELECT * FROM usuarios WHERE nombre = '" + nombre + "'"
+    cursor.execute(query)
+    return cursor.fetchall()
