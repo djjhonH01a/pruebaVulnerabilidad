51a44485acdefea7b5ac97ce3e2cfdbe40a974ed
djjhonH01a <djjhon_15@hotmail.com>
2025-11-01 18:29:36 -0500

    prueba19

--- secrets_exposed.py
+++ secrets_exposed.py
@@ -1,6 +1,6 @@
 def conectar_db_inseguro():
     import mysql.connector
-    # INSEGURO: credenciales en código1233
+    # INSEGURO: credenciales en código1
     connection = mysql.connector.connect(
         host="localhost",
         user="admin",
